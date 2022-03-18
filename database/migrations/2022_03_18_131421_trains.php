8ca2570ce4bebe42571f5d0633e16e52fd55bfa1
Immolazione <federico.deered@gmail.com>
2022-03-18 15:43:26 +0100

    small fixes

--- database/migrations/2022_03_18_131421_trains.php
+++ database/migrations/2022_03_18_131421_trains.php
@@ -16,12 +16,14 @@ class Trains extends Migration
         //
         Schema::create('trains', function (Blueprint $table){
             $table->id();
-            $table->char('azienda', 20)->nullable();   //azienda
-            $table->char('stazione_di_partenza', 30);  //stazione partenza
-            $table->char('stazione_di_arrivo', 30);  //stazione arrivo
-            $table->dateTime('orario_partenza', 0);  //ora partenza
-            $table->dateTime('orario_arrivo', 0);  //ora arrivo
-            $table->smallInteger('codice_treno');   //codice treno
+            $table->string('azienda', 20)->nullable();   //azienda
+            $table->string('stazione_di_partenza', 30);  //stazione partenza
+            $table->string('stazione_di_arrivo', 30);  //stazione arrivo
+            $table->dateTime('orario_partenza');  //ora partenza
+            $table->dateTime('orario_arrivo');  //ora arrivo
+            $table->smallInteger('codice_treno')->unsigned(); //codice treno
+            $table->boolean('treno_in_orario', true); //treno in orario
+            $table->boolean('cancellato', true); //treno in partenza
             $table->timestamps();
         });
     }
@@ -34,6 +36,6 @@ class Trains extends Migration
     public function down()
     {
         //
-        Schema::drop('trains');
+        Schema::dropIfExists('trains');
     }
 }
