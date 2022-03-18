fff5dc3fe7c6bf799f599e357a581bfcdb50fcc2
Immolazione <federico.deered@gmail.com>
2022-03-18 15:43:17 +0100

    add trains.php in config

---
+++ config/trains.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    [
+        "azienda" => "Italo",
+        "stazione_di_partenza" => "Milano",
+        "stazione_di_arrivo" => "Napoli",
+        "orario_partenza" => "",
+        "orario_arrivo" => "",
+        "codice_treno" => "09234",
+        "treno_in_orario" => true,
+        "cancellato" => false,
+    ],
+    [
+        "azienda" => "Trenord",
+        "stazione_di_partenza" => "Roma",
+        "stazione_di_arrivo" => "Milano",
+        "orario_partenza" => "",
+        "orario_arrivo" => "",
+        "codice_treno" => "34521",
+        "treno_in_orario" => false,
+        "cancellato" => true,
+    ]
+];
+
+?>
