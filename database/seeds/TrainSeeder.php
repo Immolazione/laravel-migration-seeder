0f61c2e47be9d5d32d3a34c32cfaf8e3796227c8
Immolazione <federico.deered@gmail.com>
2022-03-18 16:02:59 +0100

    try to compile seeder

--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -12,5 +12,19 @@ class TrainSeeder extends Seeder
     public function run()
     {
         //
+        $trains = config('trains');
+
+        foreach($trains as $train){
+            $newTrain = new Train();
+            $newTrain->azienda = $train['azienda'];
+            $newTrain->stazione_di_partenza = $train['stazione_di_partenza'];
+            $newTrain->stazione_di_arrivo = $train['stazione_di_arrivo'];
+            $newTrain->orario_partenza = $train['orario_partenza'];
+            $newTrain->orario_arrivo = $train['orario_arrivo'];
+            $newTrain->codice_treno = $train['codice_treno'];
+            $newTrain->treno_in_orario = $train['treno_in_orario'];
+            $newTrain->cancellato = $train['cancellato'];
+            $newTrain->save();
+        }
     }
 }
